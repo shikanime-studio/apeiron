a5a8abd54e91eb109481fe8f3562c6eefee2f5b4
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-10-01 14:46:48 +0200

    Migrate Terraform to manifests
    ghstack-source-id: 3060626137f207e502c32233b3e97eba73b4df0a
    Pull Request resolved: https://github.com/shikanime-studio/apeiron/pull/117

--- infra/apeiron-services/output.tf
+++
@@ -1,8 +0,0 @@
-output "apeiron" {
-  value = {
-    discord_token     = var.discord.token
-    google_ai_api_key = var.google_ai.api_key
-    mistral_api_key   = var.mistral.api_key
-  }
-  sensitive = true
-}
---
+++ manifests/apps/apeiron/overlays/default/update.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env nix
+#! nix develop --impure --command bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+sops \
+  --encrypt \
+  "$(dirname "$0")"/apeiron/.env > \
+  "$(dirname "$0")"/apeiron/.enc.env
