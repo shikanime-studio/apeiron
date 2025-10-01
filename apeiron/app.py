5e049fd6ba779431c9c03715fd91a72bfdd5be6d
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-10-01 14:43:33 +0200

    Add messages history
    [ghstack-poisoned]

--- apeiron/app.py
+++ apeiron/app.py
@@ -7,19 +7,22 @@ from discord import AutoShardedBot, Client, Intents, Message
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from langchain_core.runnables import RunnableConfig
+from langchain_core.messages import trim_messages
+from pydantic import BaseModel, Field
 
 import apeiron.logging
-from apeiron.agents.operator_6o import create_agent, Response
+from apeiron.chat_message_histories.discord import DiscordChannelChatMessageHistory
+from apeiron.agents.operator_6o import create_agent
 from apeiron.chat_models import create_chat_model
 from apeiron.store import create_store
 from apeiron.toolkits.discord.toolkit import DiscordToolkit
 from apeiron.tools.discord.utils import (
-    create_chat_message,
     create_configurable,
     is_bot_mentioned,
     is_bot_message,
     is_private_channel,
 )
+from apeiron.messages.utils import trim_messages_images
 
 logger = logging.getLogger(__name__)
 
@@ -76,6 +79,19 @@ def create_bot():
             return
 
         try:
+            chat_history = DiscordChannelChatMessageHistory(bot)
+            await chat_history.load_messages_from_message(message)
+            inputs = {
+                "messages": trim_messages(
+                    trim_messages_images(chat_history.messages, max_images=1),
+                    token_counter=model,
+                    strategy="last",
+                    max_tokens=2000,
+                    start_on="human",
+                    end_on=("human", "tool"),
+                    include_system=True,
+                )
+            }
             config: RunnableConfig = {
                 "configurable": create_configurable(message),
             }
@@ -83,7 +99,7 @@ def create_bot():
                 config["configurable"]["guild_id"] = message.guild.id
             async with message.channel.typing():
                 result = await graph.ainvoke(
-                    {"messages": [create_chat_message(message)]},
+                    inputs,
                     config=config,
                 )
             response: Response = result["structured_response"]
